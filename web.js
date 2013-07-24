414326bebc202f6e15e6c79f228487c111f4ed65
Mattias Holmberg <holmberm@gmail.com>
2013-07-24 13:56:54 +0200

    Further updated web.js

--- web.js
+++ web.js
@@ -3,10 +3,11 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+    var greeting = fs.readFileSync('index.html', encoding='utf8');
+    response.send(greeting);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+    console.log("Listening on " + port);
 });
